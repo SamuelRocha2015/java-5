a3da54f931ff4ac7ccebb9a616d3665dc222cee0
Samuel Rocha <samuelrodriguessr@hotmail.com>
2018-12-05 23:35:05 -0300

    project configuration

--- src/main/java/challenge/Application.java
+++ src/main/java/challenge/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @ComponentScan("challenge.QuoteRepository")
 public class Application extends SpringBootServletInitializer {
 
